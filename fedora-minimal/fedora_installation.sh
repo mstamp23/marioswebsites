e4e5f32de3469f13a8ed642b9ce5e931b561456a
mstamp23 <49061193+mstamp23@users.noreply.github.com>
2026-03-08 16:02:25 +0200

    Update fedora_installation.sh

--- fedora-minimal/fedora_installation.sh
+++ fedora-minimal/fedora_installation.sh
@@ -1,3 +1,5 @@
 sudo dnf install @xfce-desktop-environment -y   
 sudo systemctl set-default graphical.target   
 reboot
+#from machine login run the following 
+# curl -sL https://tinyurl.com/m6wfkn45 | sudo bash
