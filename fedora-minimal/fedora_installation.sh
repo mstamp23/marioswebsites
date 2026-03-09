380d65036a59d47fcc60280035ccd6e7a6b0c9ed
mstamp23 <49061193+mstamp23@users.noreply.github.com>
2026-03-09 05:37:52 +0200

    Update fedora_installation.sh

--- fedora-minimal/fedora_installation.sh
+++ fedora-minimal/fedora_installation.sh
@@ -2,3 +2,34 @@ sudo dnf install @xfce-desktop-environment -y   
 sudo systemctl set-default graphical.target   
 reboot
 
+
+sudo dnf install -y \
+@xfce-desktop-environment \
+gnome-keyring \
+firefox \
+chromium \
+ffmpeg \
+gstreamer1-plugins-{base,good,bad-free,bad-freeworld,ugly} \
+gstreamer1-libav \
+pulseaudio \
+pulseaudio-utils \
+alsa-plugins-pulseaudio \
+libdvdcss \
+rpmfusion-free-release \
+rpmfusion-nonfree-release \
+vlc \
+wget \
+curl \
+unrar \
+p7zip \
+fonts-freetype \
+fonts-dejavu \
+libvpx \
+libx264 \
+mesa-dri-drivers \
+mesa-vulkan-drivers \
+mesa-libGL \
+mesa-libGLU \
+xorg-x11-drv-{intel,nouveau,vesa} \
+NetworkManager-tui \
+nano
